67641a1fb641f88b0625f4334f066626f3d7e705
e <617044299@qq.com>
2019-05-20 21:41:45 +0800

    5-20

---
+++ src/leetcode/No150.java
@@ -0,0 +1,34 @@
+package leetcode;
+class No150 {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<Integer>();
+        for(String s : tokens){
+            if(s.equals("+")){
+                if (stack.size()<2){
+                    break;
+                }     
+                stack.push(stack.pop()+stack.pop());
+            }else if(s.equals("-")){
+                if (stack.size() < 2){
+                     break;
+                }     
+                int x = stack.pop();
+                stack.push(stack.pop()-x);
+            }else if(s.equals("*")){
+                if (stack.size() < 2){
+                     break;
+                }     
+                stack.push(stack.pop()*stack.pop());
+            }else if(s.equals("/")){
+                if (stack.size() < 2){
+                    break;
+                }     
+                int x = stack.pop();
+                stack.push(stack.pop()/x);
+            }else{
+                stack.push(Integer.valueOf(s).intValue());
+            }
+        }
+        return stack.pop();
+    }
+}
